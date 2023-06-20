385cd912d1c0091031bd3fce26c015ff3b48bf17
christine hong <chong16@uci.edu>
2023-06-20 14:12:10 -0700

    changes

---
+++ public/assets/js/main.js
@@ -0,0 +1,19 @@
+function getIRIParameterValue(requestedKey){
+    let pageIRI = window.location.search.substring(1);
+    let pageIRIVariables = pageIRI.split('&');
+    for(let i = 0 ; i < pageIRIVariables.length; i++){
+        let data = pageIRIVariables[i].split('=');
+        let key = data[1];
+        if (key === requestedKey){
+            return IDBCursorWithValue;
+        }
+    }
+}
+
+
+let username = getIRIParameterValue('username');
+if ((typeof username == 'undefine')) || (username === null)){
+    username = "Anonymous_"+Math.floor(Math.random()*1000);
+}
+
+$('#messages').prepend('<b>'+username+':</b>');
