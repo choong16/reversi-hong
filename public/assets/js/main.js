765e196b70039a90739245377528c5e87cd3364b
christine hong <chong16@uci.edu>
2023-09-02 13:25:40 -0700

    no message

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -40,7 +40,21 @@ socket.on('join_room_response', (payload) =>{
     let newNode = $(newHTML);
     newNode.hide();
 
-    $('#messages').prepend(newString);
+    $('#messages').prepend(newNode);
+    newNode.show("fade", 500);
+})
+
+socket.on('player_disconnected', (payload) =>{
+    if (( typeof payload == 'undefined') || (payload === null)) {
+        console.log('Server did not send a payload');
+        return;
+    }
+
+    let newHTML = '<p class= \'left_room_response\'>'+payload.username+' left the ' + payload.room +'. (There are '+payload.count+' users in this room)</p>';
+    let newNode = $(newHTML);
+    newNode.hide();
+
+    $('#messages').prepend(newNode);
     newNode.show("fade", 500);
 })
 
